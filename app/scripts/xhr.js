0e915815d588e20b81da16a1c4e73c5a5853b4d5
Alec Flett <alecf@flett.org>
2015-02-02 21:02:45 -0800

    move xhr out

---
+++ app/scripts/xhr.js
@@ -0,0 +1,54 @@
+// Create an XHR context that fires off progress notifications
+// usage:
+// function update_progress(waiting, total) {
+//   console.log("Updated ", waiting, " / ", total);
+// }
+// var XHR = xhrContext(update_progress);
+// XHR('http://....).then(function(result)) {... });
+function XHRContext(progress) {
+    var total_started = 0;
+    var total_complete = 0;
+
+    function update_progress() {
+        progress(total_started, total_complete);
+        if (total_started == total_complete)
+            total_started = total_complete = 0;
+    }
+
+    return function(url) {
+        total_started += 1;
+        progress(total_started, total_complete);
+        var xhr = new XMLHttpRequest();
+        var p = new Promise(function(resolve, reject) {
+            try {
+                LASTXHR = xhr;
+                xhr.open('GET', url, true);
+                xhr.onload = function() {
+                    // console.error("Got response from ", url, ": ", xhr.responseText);
+                    try {
+                        var response = JSON.parse(xhr.responseText);
+                        resolve(response);
+                    } catch(ex) {
+                        console.trace(ex, " in ", xhr.responseText);
+                        reject(ex);
+                    }
+                    total_complete += 1;
+                    update_progress();
+                };
+                xhr.onloadend = function(e) {
+                    total_complete += 1;
+                    update_progress();
+                    reject(e);
+                };
+                //console.log("Requesting ", url);
+                xhr.send();
+            } catch (ex) {
+                total_complete +=1;
+                update_progress();
+                reject(ex);
+            };
+        });
+        p.request = xhr;
+        return p;
+    };
+}
